c45dd31a8296d4ac3f8216eb4781679bc988a1a8
HariHaran9209 <90740755+HariHaran9209@users.noreply.github.com>
2025-05-18 18:06:20 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,9 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    port: 5173, // or leave it out; Render sets it via $PORT
+    host: true,
+    allowedHosts: ['shop-mash.onrender.com'],
+  },
 })
